8882d7ffba707024587c25866b800d42c2822e4f
BJKim1 <130324349+BJKim1@users.noreply.github.com>
2023-04-13 16:17:20 +0900

    Update testmytranspose.R
    I did my part.

--- testmytranspose.R
+++ testmytranspose.R
@@ -7,3 +7,20 @@ myvar1_test_cases <- list(
   list(matrix(c(1,2), nrow=1, ncol=2), matrix(c(1,2), nrow=2, ncol=1)),
   list(matrix(c(1,2), nrow=2, ncol=1), matrix(c(1,2), nrow=1, ncol=2))
 )
+
+# Define the function to test
+mytranspose <- function(x) {
+  if (is.vector(x)) {   # for vectors
+    y <- matrix(x, nrow = 1, ncol = length(x))
+  } else if (is.data.frame(x)) {   # for data frames
+    y <- as.matrix(x)
+  } else {   # for matrices
+    y <- matrix(1, nrow = ncol(x), ncol = nrow(x))
+    for (i in 1:nrow(x)) {
+      for (j in 1:ncol(x)) {
+        y[j,i] <- x[i,j]
+      }
+    }
+  }
+  return(y)
+}
